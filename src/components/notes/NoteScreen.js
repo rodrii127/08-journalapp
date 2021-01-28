6240bf7bc7fc5c52f06d8da55d41630baf2f9cb7
Rodrigo Rodriguez <rodrigo.rodriguez@marandu.com.ar>
2021-01-28 07:22:22 -0300

    commit dia 28-01

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,23 @@ export const NoteScreen = () => {
     return (
         <div className='notes__main-content'>
             <NotesAppBar />
+
+            <div className='notes__content'>
+                
+                <input 
+                    type='text' 
+                    placeholder='Some awesome tittle' 
+                    className='notes__title-input' />
+                
+                <textarea 
+                    placeholder='What happen today?' 
+                    className='notes__textarea'></textarea>
+                
+                <div className='notes__image'>
+                    <img src='https://i.pinimg.com/originals/06/71/9e/06719e446d9f1e81a60828add84563f9.jpg' alt='imagen' />
+                </div>
+            
+            </div>
         </div>
     )
 }
