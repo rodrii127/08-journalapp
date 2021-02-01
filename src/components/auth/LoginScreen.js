63f272f94ea5641b20cedb3d23f6e070b15305c5
rodrigo <rodriguezrodrigo127@gmail.com>
2021-02-01 08:42:54 -0300

    'b'

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -5,7 +5,7 @@ export const LoginScreen = () => {
     return (
         <div> 
             <h3 className='auth__title'>Login</h3>
-
+ 
             <form>
                 <input type='text' placeholder='Email' name='email' className='auth__input' autoComplete='off'/>
                 <input type='password' placeholder='Password' name='password' className='auth__input' />
