ccbb484a6671a686db86aae4e0f1b2f8fd69b85b
rodrigo <rodriguezrodrigo127@gmail.com>
2021-02-01 18:11:00 -0300

    advances in RegisterScreen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+    
+    const [ formValues, handleInputChange ] = useForm({
+        name: 'Rodrigo',
+        email: 'rodrigo.rodriguez@gmail.com',
+        password: '1234',
+        password2: '1234'
+    }); 
+
+    const { name, email, password, password2 } = formValues;
+    
+    const handleRegister = (e) =>{
+        e.preventDefault();
+        if ( isFormValid() ) {
+            console.log('son iguales');
+        }
+    }
+
+    const isFormValid = () =>{
+        //TODOs
+    }
+
     return (
         <div>
             <h3 className='auth__title'>Register</h3>
 
-            <form>
-                <input type='text' placeholder='Name' name='name' className='auth__input' autoComplete='off'/>
-                <input type='text' placeholder='Email' name='email' className='auth__input' autoComplete='off'/>
-                <input type='password' placeholder='Password' name='password' className='auth__input' />
-                <input type='password' placeholder='Confirm password' name='password2' className='auth__input' />
+            <form onSubmit = { handleRegister }>
+                <input type='text' placeholder='Name' name='name' className='auth__input' autoComplete='off' value={name} onChange= {handleInputChange} />
+                <input type='text' placeholder='Email' name='email' className='auth__input' autoComplete='off' value={email} onChange= {handleInputChange} />
+                <input type='password' placeholder='Password' name='password' className='auth__input' value={password} onChange= {handleInputChange}/>
+                <input type='password' placeholder='Confirm password' name='password2' className='auth__input' value={password2} onChange= {handleInputChange}/>
                 <button type='submit' className='btn btn-primary btn-block mb-5'> Register </button>
 
                 <Link to='/auth/login' className='link'>Already register?</Link>
